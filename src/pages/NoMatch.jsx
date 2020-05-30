ba3c19f60b8238766445d84c5afbd00be4f86279
Lushu123 <211720950@qq.com>
2020-05-30 13:10:48 +0800

    日常提交保存

--- src/pages/NoMatch.jsx
+++ src/pages/NoMatch.jsx
@@ -1,12 +1,15 @@
 import React,{Component} from 'react'
-
+import { Result, Button } from 'antd';
 export default class NoMatch extends Component{
 
     render() {
         return(
-            <div>
-                404
-            </div>
+            <Result
+                status="404"
+                title="404"
+                subTitle="抱歉, 你访问的页面不存在。"
+                extra={<Button type="primary" onClick={this.props.history.push('/')}>回到主页</Button>}
+            />
         )
     }
 }
